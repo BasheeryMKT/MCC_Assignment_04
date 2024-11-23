01942e34199927783fe8dcbef887bd37cfe05071
BasheeryMKT <154669750+BasheeryMKT@users.noreply.github.com>
2024-11-23 18:59:30 +0000

    Updated

--- animalgame/src/App.js
+++ animalgame/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { animals } from './data/AnimalsDb';  
+import AnimalName from './Components/AnimalName';
+import SelectAnimal from './Components/SelectAnimal';
+import Result from './Components/Result';
 
 function App() {
+  const [displayedAnimal, setDisplayedAnimal] = useState("");
+  const [result, setResult] = useState("");  
+
+  useEffect(() => {
+    generateRandomAnimal(); 
+  }, []);
+
+  const generateRandomAnimal = () => {
+    const randomIndex = Math.floor(Math.random() * animals.length);
+    setDisplayedAnimal(animals[randomIndex].name);
+  };
+
+  const handleSelectAnimal = (selectedAnimal) => {
+    if (selectedAnimal === displayedAnimal) {
+      setResult('win');
+    } else {
+      setResult('lose');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 className='heading'>ANIMAL MATCHING GAME</h1>
+      </div>
+      <div className="components-container">
+        <div className="component">
+          <Result result={result} />
+        </div>
+        <div className="component">
+          <AnimalName name={displayedAnimal} />
+        </div>
+        <div className="component select-animal">
+          <SelectAnimal onSelectAnimal={handleSelectAnimal} />
+        </div>
+      </div>
     </div>
   );
 }
